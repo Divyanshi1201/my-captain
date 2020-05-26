5c79a36942841354e3e96ce0542bc783d899bd28
Divyanshi1201 <64720938+Divyanshi1201@users.noreply.github.com>
2020-05-26 12:06:41 +0530

    completed dataset machine leaning project

--- dataset_classification.py
+++ dataset_classification.py
@@ -21,17 +21,14 @@ from sklearn.ensemble import VotingClassifier
 
 
 
+
+
 #loading dataset using pandas
-df=read_csv(r"C:\Users\Kishore\Downloads\iris.csv")
+df=read_csv(r"C:\Users\MUTHU PS\Downloads\iris.csv")
+
 
 print(df.shape)
-
-df.drop(["Id"],axis=1,inplace=True)
-
 df
-
-print(df.head(20))
-
 
 
 print(df.describe())
@@ -39,16 +36,22 @@ print(df.describe())
 
 print(df.groupby("Species").size())
 
+
 #visualization of data
+%matplotlib inline
 df.plot(kind="box", color="b")
 plt.show()
+
+
 
 df.hist()
 plt.show()
 
+
 #multivariate plots
 scatter_matrix(df)
 plt.show()
+
 
 #splitting data to create a validation dataset
 x=df.iloc[:,0:4]
@@ -66,18 +69,31 @@ models.append(("SVM",SVC(gamma="auto")))
 
 
 
-
-
 #evaluate the created models
 results=[]
 names=[]
 for name,model in models:
-    kfold=StratifiedKFold(n_splist=10,rand,random_state=1)
-    cv_results=cross_val_score(models,x_train,y_train, cv=kfold, scoring="accuracy")
+    kfold=StratifiedKFold(n_splits=10,random_state=1,shuffle=True)
+    cv_results=cross_val_score(model,x_train,y_train,scoring="accuracy",cv=kfold)
     results.append(cv_results)
     names.append(name)
-    print(name,cv_results.mean(),cv_results.std())
+    print('%s: %f (%f)'%(name,cv_results.mean(),cv_results.std()))
+
+#compare our models
+plt.boxplot(results,labels=names)
+plt.title("algorithm comparison")
+plt.show()
 
 
 
- 
+
+#made predictions on gaussian naive bayes
+model=GaussianNB()
+model.fit(x_train,y_train)
+predictions=model.predict(x_test)
+
+#evaluate our predictions
+print(accuracy_score(y_test,predictions))
+print(confusion_matrix(y_test,predictions))
+print(classification_report(y_test,predictions))
+
